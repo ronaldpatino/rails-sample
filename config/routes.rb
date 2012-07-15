4d9b4d22e98bd6efa5893f2fb6f9187ce0687d0a
ronaldpatino <ronaldpatino@gmail.com>
2012-07-15 09:02:16 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DemoApp::Application.routes.draw do
+  get "users/new"
+
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
@@ -7,6 +9,15 @@ DemoApp::Application.routes.draw do
   resources :microposts
 
   resources :users
+
+
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  match '/signup', to: 'users#new'
+
+
+  root to: 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
